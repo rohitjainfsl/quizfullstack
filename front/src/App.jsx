c17a889b1782b47f02e913a2390aabdda87e8b47
rohitjainfsl <rohit@fullstacklearning.com>
2023-05-24 18:40:36 +0530

    commit 2

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Signup />}></Route>
+          <Route path="/user/signup" element={<Signup />}></Route>
           <Route path="/login" element={<Login />}></Route>
         </Routes>
       </BrowserRouter>
