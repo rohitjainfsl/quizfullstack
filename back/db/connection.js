8ddca9f4d0394f26250879ecd9efd990f2dca30f
rohitjainfsl <rohit@fullstacklearning.com>
2025-02-05 12:23:03 +0530

    deleted db folder to sync the repos

--- back/db/connection.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-const connection = mongoose.connect('mongodb+srv://mongodbuser:zFMYcFgVV4SS7jA6@cluster0.4ont6qs.mongodb.net/?retryWrites=true&w=majority')
-
-export default connection
